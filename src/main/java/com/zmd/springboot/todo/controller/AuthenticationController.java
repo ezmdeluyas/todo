1bf8ad9d6c8569f269269b85db1b306fbf957c7d
ezmdeluyas <ezdeluyas@gmail.com>
2026-02-02 19:07:32 +0800

    chore: added login functionality

--- src/main/java/com/zmd/springboot/todo/controller/AuthenticationController.java
+++ src/main/java/com/zmd/springboot/todo/controller/AuthenticationController.java
@@ -1,12 +1,15 @@
 package com.zmd.springboot.todo.controller;
 
+import com.zmd.springboot.todo.dto.request.AuthenticationRequest;
 import com.zmd.springboot.todo.dto.request.RegisterRequest;
+import com.zmd.springboot.todo.dto.response.AuthenticationResponse;
 import com.zmd.springboot.todo.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,7 +27,14 @@ public class AuthenticationController {
     @Operation(summary = "Register a user", description = "Create new user in database")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
-    public void register(@Valid @RequestBody RegisterRequest registerRequest) throws Exception{
+    public void register(@Valid @RequestBody RegisterRequest registerRequest) throws Exception {
         authenticationService.register(registerRequest);
+    }
+
+    @Operation(summary = "Login a user", description = "Submit email & password to authenticate user")
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request) {
+
+        return ResponseEntity.ok(authenticationService.login(request));
     }
 }
